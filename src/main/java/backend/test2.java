659296b6d723c6b5bc9234a78075c2a338a2ec43
ismailnjb <mohammed.ismail.njb@gmail.com>
2024-08-21 13:20:19 +0530

    dd

--- src/main/java/backend/test2.java
+++ src/main/java/backend/test2.java
@@ -2,6 +2,6 @@ package backend;
 
 public class test2 {
      public static void main(String[] args){
-         System.out.println("hello form test 2");
+         System.out.println("hello form test 2   and 2");
      }
 }
